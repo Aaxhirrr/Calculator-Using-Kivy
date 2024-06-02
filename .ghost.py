eb395cac95027ac594dc7999199c927dff80c557
Aaxhirrr <anola133@asu.edu>
2024-06-02 12:41:01 -0700

    Fix float formatting issue in output

--- .ghost.py
+++ .ghost.py
@@ -19,3 +19,4 @@
 # Add comments to clarify Kivy structure 13945
 # Add comments to clarify Kivy structure 25838
 # Improve font rendering in display 29112
+# Fix float formatting issue in output 11450
